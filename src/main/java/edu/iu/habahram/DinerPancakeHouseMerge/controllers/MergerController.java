1671b1bbbfe74b7229dbcf430b5536547d3ba0c8
Jack-Donahue <jfdonahu@iu.edu>
2024-04-10 13:49:05 -0400

    Testing Homework 7

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,12 +1,14 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
+import edu.iu.habahram.DinerPancakeHouseMerge.model.CafeMenu;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItemRecord;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,53 @@ public class MergerController {
     public List<MenuItemRecord> get() {
         List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
         return items;
+    }
+
+    @GetMapping("/vegetarian")
+    public List<MenuItemRecord> getVegetarianItems() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        List<MenuItemRecord> vegetarianItems = new ArrayList<>();
+        for (MenuItemRecord item : items) {
+            if (item.vegetarian()) {
+                vegetarianItems.add(item);
+            }
+        }
+        return vegetarianItems;
+    }
+
+    @GetMapping("/breakfast")
+    public List<MenuItemRecord> getBreakfastItems() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        List<MenuItemRecord> breakfastItems = new ArrayList<>();
+        for (MenuItemRecord item : items) {
+            if (item.description() == "Breakfast") {
+                breakfastItems.add(item);
+            }
+        }
+        return breakfastItems;
+    }
+
+    @GetMapping("/lunch")
+    public List<MenuItemRecord> getLunchItems() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        List<MenuItemRecord> lunchItems = new ArrayList<>();
+        for (MenuItemRecord item : items) {
+            if (item.description() == "Lunch") {
+                lunchItems.add(item);
+            }
+        }
+        return lunchItems;
+    }
+
+    @GetMapping("/supper")
+    public List<MenuItemRecord> getDinnerItems() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        List<MenuItemRecord> dinnerItems = new ArrayList<>();
+        for (MenuItemRecord item : items) {
+            if (item.description() == "Dinner") {
+                dinnerItems.add(item);
+            }
+        }
+        return dinnerItems;
     }
 }
